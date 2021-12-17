4e28a1a46c9367bb2a9e6ac0f2e611d9f98954df
Вадим <vados1.95@mail.ru>
2021-12-17 19:07:38 +0300

    add Blanchard

--- JS/Blanchard.js
+++ JS/Blanchard.js
@@ -1,3 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  allowTouchMove: false,
+  loop: true,
+  effect: 'fade',
+  speed: 10000,
+  autoplay: {
+    delay: 10000
+  }
+});
+
 const params = {
   btnClassName: "main__item-btn",
   activeClassName: "is-active",
@@ -44,3 +54,82 @@ function setMenuListener() {
 
 setMenuListener();
 
+const element = document.querySelector('select');
+const choices = new Choices(element, {
+      searchEnabled: false,
+      itemSelectText: ''
+});
+
+
+
+let gallerySlider = new Swiper(".slides-container", {
+  slidesPerView: 1,
+  grid: {
+    rows: 1,
+    fill: "row"
+  },
+  spaceBetween: 20,
+  pagination: {
+    el: ".section-gallery .section-gallery__pagination",
+    type: "fraction"
+  },
+  navigation: {
+    nextEl: ".section-gallery__next",
+    prevEl: ".section-gallery__prev"
+  },
+
+  breakpoints: {
+    441: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 30
+    },
+
+    1200: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 50
+    }
+  },
+
+  a11y: false,
+  keyboard: true, // можно управлять с клавиатуры стрелками влево/вправо
+
+  // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
+  watchSlidesProgress: true,
+  slideVisibleClass: 'slide-visible',
+
+  on: {
+    init: function () {
+      this.slides.forEach(slide => {
+        if (!slide.classList.contains('slide-visible')) {
+          slide.tabIndex = '-1';
+        } else {
+          slide.tabIndex = '';
+        }
+      });
+    },
+    slideChange: function () {
+      this.slides.forEach(slide => {
+        if (!slide.classList.contains('slide-visible')) {
+          slide.tabIndex = '-1';
+        } else {
+          slide.tabIndex = '';
+        }
+      });
+    }
+  }
+
+  // on: {
+  //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+  //   beforeResize: function () {
+  //     this.slides.forEach((el) => {
+  //       el.style.marginTop = "";
+  //     });
+  //   }
+  // }
+});
